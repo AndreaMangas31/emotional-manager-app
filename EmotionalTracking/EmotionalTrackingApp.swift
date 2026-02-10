2d04da54a203ec32a0a1d76e4eed6fb03f15bb9d
AndreaMangas31 <andream3103@gmail.com>
2026-02-10 22:29:27 +0100

    Initial Commit

---
+++ EmotionalTracking/EmotionalTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionalTrackingApp.swift
+//  EmotionalTracking
+//
+//  Created by Andrea Mangas Torres on 10/2/26.
+//
+
+import SwiftUI
+
+@main
+struct EmotionalTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
